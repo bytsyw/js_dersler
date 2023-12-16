c959f44a3a2ffcffeeef9f2b8fe2dc2ea3f01079
Beytullah Soy <beyt649@gmail.com>
2023-12-16 17:36:50 +0300

    Değişken türleri ile string number ve booleani öğrendik

--- js_temelleri.js
+++ js_temelleri.js
@@ -1,9 +1,57 @@
 // console.log(5000 + (5000 * 0.3));
 // console.log(6000 + (5000 * 0.3)); // Değişkensiz tanımlama
 
-var maasAli = 7000;
-var maasCan = 6000;
-var zam = 0.3;
+// var maasAli = 7000;
+// var maasCan = 6000;
+// var zam = 0.3;
 
-console.log(maasAli + (maasAli * zam));
-console.log(maasCan + (maasCan * zam));
+// console.log(maasAli + (maasAli * zam));
+// console.log(maasCan + (maasCan * zam));  //Değişkenler ile konsola çıktı alma
+
+//Türkçe karakter kullanılmaz
+//Arada boşluk bırakılmaz
+//Sayı ile başlayamaz
+//Kendine ait yazım şekli ile tek düze yazmalısın kod okunabilirliği açısından
+
+
+
+                                //Değişken Türleri
+
+
+// var urunAdi = "iphone 13"; //String
+// let urunFiyati = "15000"; // String: Tırnak içine aldığımız herşey string dir.
+
+// let sayi2 = "10";
+// let sayi1 = "20"; // String bir ifadeyi içinde sadece sayı değeri var ise bu değişkeni sayıya çevirmek için Number() i kullanıyoruz
+
+// console.log(Number(sayi1) + Number(sayi2));
+
+// let sayi2 = 10;
+// let sayi1 = 20; // Number bir değişkende veriyi stringe çevirmek için toString() komutunu kullanıyoruz
+
+// console.log(sayi1.toString() + sayi2.toString());
+
+
+// let isim = "Beytullah";
+// let soyad = "Soy";  " " karakteri ile bir değişkene verilen kelimeleri ayırmak için bu karakter kullanılabilir tek + operatörü ile string değişkenler birleştirilir integer değişkenler toplanır
+
+// console.log(isim + " " + soyad)
+
+// let sinavNotu = 70;
+// let basarilimi = (sinavNotu >50); 
+
+// console.log(basarilimi)
+// console.log(typeof basarilimi) // Boolean veri türü true veya false dönderir 
+
+
+// let yas;
+
+// console.log(yas);
+// console.log(typeof yas); // Burada yas içinde bir veri yok ike veri undefined döner 
+
+// yas = "";
+// console.log(yas);
+// console.log(typeof yas); // Burada ise yas'a bir "" gönderirken bunu karakter ve yas'ı içi dolu olarak görür ve bu değer string olur 
+
+
+
