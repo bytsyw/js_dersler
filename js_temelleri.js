327a5ebffc30a3df32127c1aa0bfb15f0cca4eda
Beytullah Soy <beyt649@gmail.com>
2023-12-16 15:44:48 +0300

    degiskenler ile kisinin maasini ve zam oranini kolaylastirma

--- js_temelleri.js
+++ js_temelleri.js
@@ -1,1 +1,9 @@
-void
+// console.log(5000 + (5000 * 0.3));
+// console.log(6000 + (5000 * 0.3)); // Değişkensiz tanımlama
+
+var maasAli = 7000;
+var maasCan = 6000;
+var zam = 0.3;
+
+console.log(maasAli + (maasAli * zam));
+console.log(maasCan + (maasCan * zam));
