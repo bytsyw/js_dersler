648c8df54fec367af18660fbef3fa2f1554bc991
Beytullah Soy <114873625+bytsyw@users.noreply.github.com>
2024-01-29 15:28:38 +0300

    multiple kullanımlarını öğreniyoruz

--- js_domEvents.js
+++ js_domEvents.js
@@ -1,22 +1,51 @@
-let sonuc;
+                    //single-elements
 
-sonuc = document.getElementById("title");
-sonuc = document.getElementById("title").className;
-sonuc = document.getElementById("title").classList;
+// let sonuc;
 
-document.getElementById("title").style.fontSize ="33px";
-document.getElementById("title").style.color ="Red";   // Burada getElementById ile ilgili elementin id sini kullanarak stillerine erişebiliyoruz böylelikle script üzerinden de sayfa içerisinde değişiklikler yapabiliyoruz
+// sonuc = document.getElementById("title");
+// sonuc = document.getElementById("title").className;
+// sonuc = document.getElementById("title").classList;
 
-document.getElementById("title").innerText ="TodoApp 2";
-document.getElementById("title").innerHTML ="<p>TodoApp 2</p>"; // innerhtml nin innertext ten farkı içerisinde html kodları kullanabiliyoruz ilgili id nin 
+// document.getElementById("title").style.fontSize ="33px";
+// document.getElementById("title").style.color ="Red";   // Burada getElementById ile ilgili elementin id sini kullanarak stillerine erişebiliyoruz böylelikle script üzerinden de sayfa içerisinde değişiklikler yapabiliyoruz
 
-sonuc = document.querySelector("#title"); 
-sonuc = document.querySelector(".card-header"); 
-sonuc = document.querySelector("div");     // burada querySelector ile hem id ye  hemde classlara erişebiliyoruz aynı css gibi özellik verebiliyoruz 
+// document.getElementById("title").innerText ="TodoApp 2";
+// document.getElementById("title").innerHTML ="<p>TodoApp 2</p>"; // innerhtml nin innertext ten farkı içerisinde html kodları kullanabiliyoruz ilgili id nin 
 
-sonuc = document.querySelector("li");     
-sonuc = document.querySelector("li:first-child");
-sonuc = document.querySelector("li:last-child");
-sonuc = document.querySelector("li:nth-child(2)");
+// sonuc = document.querySelector("#title"); 
+// sonuc = document.querySelector(".card-header"); 
+// sonuc = document.querySelector("div");     // burada querySelector ile hem id ye  hemde classlara erişebiliyoruz aynı css gibi özellik verebiliyoruz 
 
-console.log(sonuc);
+// sonuc = document.querySelector("li");     
+// sonuc = document.querySelector("li:first-child");
+// sonuc = document.querySelector("li:last-child");
+// sonuc = document.querySelector("li:nth-child(2)");
+
+// console.log(sonuc);
+
+
+
+
+
+// let sonuc;
+
+// sonuc = document.getElementsByClassName("task")[0];   
+// sonuc = document.getElementsByClassName("task")[1];
+
+// taskList = document.getElementsByClassName("task");
+// ul = document.getElementById("task-list");
+// taskList = ul.getElementsByTagName("li");
+
+// taskList = document.querySelectorAll("#task-list li"); // Burada bu etiket ile yukarıda iki işlem yapmak yerine tek satırda css kullanır gibi yazarak kapsayıcı yapabiliyoruz
+
+// for(let i = 0;i<taskList.length;i++){
+//     console.log(taskList)
+// }
+
+// for(let task of taskList){
+//     task.style.color="red"
+//     task.style.fontSize="20px"
+//     console.log(taskList)
+// }
+
+// console.log(sonuc);
