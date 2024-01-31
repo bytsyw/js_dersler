4dd334286a60a8fde137e3d7deb40a1b938688a1
Beytullah Soy <114873625+bytsyw@users.noreply.github.com>
2024-01-31 13:43:10 +0300

    Bu committe herhangi bir butona basmadan klavyeden enter tuşuna basıldığında bir olayı nasıl kayıt ederiz (Keypress) onu öğrendik

--- js_domEvents.js
+++ js_domEvents.js
@@ -176,41 +176,50 @@
 // event.preventDefault();
 // } // Burada eventin özelliklerine erişim sağladığımızda ne oluyor onu öğrendik burada fonksiyonu çağırdığımız yerdeki class a btn-lg veya herhangi bir class özelliği verirsek o ekleniyor
 
-let gorevListesi = [
-  { id: 1, gorevAdi: "Görev 1" },
-  { id: 2, gorevAdi: "Görev 2" },
-  { id: 3, gorevAdi: "Görev 3" },
-  { id: 4, gorevAdi: "Görev 4" },
-];
-displayTask();
-function displayTask() {
-  ul = document.getElementById("task-list");
-  ul.innerHTML = "";
-  for (let gorev of gorevListesi) {
-    let li = `<li class="task list-group-item">
-                    <div class="form-check">
-                        <input type="checkbox" name="" id="${gorev.id}" class="form-check-input">
-                        <label for="${gorev.id}" class="form-check-label">${gorev.gorevAdi}</label>
-                    </div>
-                </li>`;
-    ul.insertAdjacentHTML("beforeend", li);
-  }
-}
 
-document.querySelector("#btnAddNewTask").addEventListener("click", newTask);
+            // Listeye eleman nasıl eklenir 
 
-function newTask(event) {
-  let taskInput = document.querySelector("#txtTaskName");
-  if (taskInput.value == "") {
-    alert("Görev giriniz");
-  } else {
-    gorevListesi.push({
-      id: gorevListesi.length + 1,
-      gorevAdi: taskInput.value,
-    });
-    taskInput.value ="";
-    displayTask();
-  }
 
-  event.preventDefault();
-}
+// let gorevListesi = [
+//   { id: 1, gorevAdi: "Görev 1" },
+//   { id: 2, gorevAdi: "Görev 2" },
+//   { id: 3, gorevAdi: "Görev 3" },
+//   { id: 4, gorevAdi: "Görev 4" },
+// ];
+// displayTask();
+// function displayTask() {
+//   ul = document.getElementById("task-list");
+//   ul.innerHTML = "";
+//   for (let gorev of gorevListesi) {
+//     let li = `<li class="task list-group-item">
+//                     <div class="form-check">
+//                         <input type="checkbox" name="" id="${gorev.id}" class="form-check-input">
+//                         <label for="${gorev.id}" class="form-check-label">${gorev.gorevAdi}</label>
+//                     </div>
+//                 </li>`;
+//     ul.insertAdjacentHTML("beforeend", li);
+//   }
+// }
+
+// document.querySelector("#btnAddNewTask").addEventListener("click", newTask);
+// document.querySelector("btnNewTask").addEventListener("keypress",function(){
+//     if(event.key == "Enter"){
+//         document.getElementById("#btnAddNewTask").click();
+//     }
+// })
+
+// function newTask(event) {
+//   let taskInput = document.querySelector("#txtTaskName");
+//   if (taskInput.value == "") {
+//     alert("Görev giriniz");
+//   } else {
+//     gorevListesi.push({
+//       id: gorevListesi.length + 1,
+//       gorevAdi: taskInput.value,
+//     });
+//     taskInput.value ="";
+//     displayTask();
+//   }
+
+//   event.preventDefault();
+// }
