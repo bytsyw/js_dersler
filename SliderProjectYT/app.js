8850d21c2f20cd1392f941dbd2a8ca20840337e4
Beytullah Soy <114873625+bytsyw@users.noreply.github.com>
2024-02-10 17:13:31 +0300

    Html ve js te düzenlemeler yapıldı.
    Artık html nin içine çokça html kodu yazmak yerine js te bir obje dizisi oluşturduk ve buraya her veri eklediğimizde otomatik olarak ana sayfada yeni veri gözükecektir bunu yapmamızın sebebi siteyi statik ten dinamiğe çevirmemiz yakın zamanda node.js ile api bağlantıları yapıp veriyi admin panelinden ekleyebileceğiz.

--- SliderProjectYT/app.js
+++ SliderProjectYT/app.js
@@ -5,28 +5,38 @@ var model = [
         "img":"img/audi.jpg",
         "author":"BYT",
         "description":"description",
+        "des":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?",
+        "topic":"Car",
         "title":"Audi",
     },
     {
         "img":"img/dodgeChalenger.jpg",
         "author":"BYT",
         "description":"description",
+        "des":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?",
+        "topic":"Car",
         "title":"Dodge",
     },
     {
         "img":"img/ferrari.jpg",
         "author":"BYT",
+        "des":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?",
         "description":"description",
+        "topic":"Car",
         "title":"ferrari",
     },
     {
         "img":"img/fordMustang.jpg",
         "description":"description",
+        "des":"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?",
+        "topic":"Car",
         "title":"Ford",
     }
 ]
 
+
 var div = document.getElementById('Thumbnail');
+var div1 = document.getElementById('List');
 
 for(let models of model){
     let list = `
@@ -41,7 +51,21 @@ for(let models of model){
         </div>
     </div>
 </div>`
+let baseList = `<div class="item">
+<img src="${models.img}">
+<div class="content">
+    <div class="author">${models.author}</div>
+    <div class="title">${models.title}</div>
+    <div class="topic">${models.topic}</div>
+    <div class="des">${models.des}</div>
+    <div class="buttons">
+        <button>SEE MORE</button>
+        <button>SUBSCRIBE</button>
+    </div>
+</div>
+</div>`
 div.insertAdjacentHTML('beforeend',list);
+div1.insertAdjacentHTML('beforeend',baseList);
 }
 console.log(div)
 
