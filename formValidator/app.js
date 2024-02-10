246f80d372917c79c2b364b77c7902b0bd03a363
Beytullah Soy <114873625+bytsyw@users.noreply.github.com>
2024-02-10 14:35:25 +0300

    E mail sisteminin getirilmesi alanların doluluk boşluklarına göre hata mesajı verilmesi veya onaylanması

--- formValidator/app.js
+++ formValidator/app.js
@@ -5,19 +5,57 @@ const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 
 
+function error(input, message){
+    input.className = 'form-control is-invalid';
+    const div = input.nextElementSibling;
+    div.innerText = message;
+    div.className = 'invalid-feedback'
+}
+function success(input){
+    input.className('form-control is-valid')
+}
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
 form.addEventListener('submit',function(e){
     e.preventDefault();
 
     if(username.value ===''){
-        username.className = 'form-control is-invalid';
+        error(username, "Username Gerekli");
+    }
+    else{
+        success(username)
     }
     if(email.value ===''){
-        email.className = 'form-control is-invalid';
+        error(email, "E-mail Gerekli");
+    }
+    else if(!validateEmail(email.value)){
+        error(email," E-mail adresinizi lütfen kontrol ediniz")
+    }
+    else{
+       success(email)
+
     }
     if(password.value ===''){
-        password.className = 'form-control is-invalid';
+        error(password, "password Gerekli");
+
+
+    }
+    else{
+       success(password)
+
     }
     if(repassword.value ===''){
-        repassword.className = 'form-control is-invalid';
+        error(repassword, "repassword Gerekli");
+    }
+    else{
+       success(repassword)
+
     }
 })
