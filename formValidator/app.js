a8ef7c15047c9c722a4285d90154889a24abad73
Beytullah Soy <114873625+bytsyw@users.noreply.github.com>
2024-02-10 14:50:20 +0300

    birden çok if kullanarak kontrol etmek yerine tek bir if else içine yazıp kontrol ediyoruz

--- formValidator/app.js
+++ formValidator/app.js
@@ -15,47 +15,65 @@ function success(input){
     input.className('form-control is-valid')
 }
 
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
+function checkEmail(input){
+    const re =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  if (re.test(input.value)){
+    success(input)
+  }
+  else{
+    error(input,'hatalı bir mail adresi');
+  }
+}
+
+  function checkRequired(inputs){
+    inputs.forEach(function(input){
+        if(input.value ===''){
+        error(input, `${input.id} is required`);
+    }
+        else{
+        success(input)
+    }
+    });
+    
+  }
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
 
-    if(username.value ===''){
-        error(username, "Username Gerekli");
-    }
-    else{
-        success(username)
-    }
-    if(email.value ===''){
-        error(email, "E-mail Gerekli");
-    }
-    else if(!validateEmail(email.value)){
-        error(email," E-mail adresinizi lütfen kontrol ediniz")
-    }
-    else{
-       success(email)
+    checkRequired([username,email,password,repassword]);
+    checkEmail(email);    
 
-    }
-    if(password.value ===''){
-        error(password, "password Gerekli");
+    // if(username.value ===''){
+    //     error(username, "Username Gerekli");
+    // }
+    // else{
+    //     success(username)
+    // }
+    // if(email.value ===''){
+    //     error(email, "E-mail Gerekli");
+    // }
+    // else if(!validateEmail(email.value)){
+    //     error(email," E-mail adresinizi lütfen kontrol ediniz")
+    // }
+    // else{
+    //    success(email)
+
+    // }
+    // if(password.value ===''){
+    //     error(password, "password Gerekli");
 
 
-    }
-    else{
-       success(password)
+    // }
+    // else{
+    //    success(password)
 
-    }
-    if(repassword.value ===''){
-        error(repassword, "repassword Gerekli");
-    }
-    else{
-       success(repassword)
+    // }
+    // if(repassword.value ===''){
+    //     error(repassword, "repassword Gerekli");
+    // }
+    // else{
+    //    success(repassword)
 
-    }
+    // }
 })
